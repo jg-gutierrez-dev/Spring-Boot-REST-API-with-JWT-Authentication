1127d2699a4d31c911ddbe7a655df68cbccf406e
jg-gutierrez-dev <jg.gutierrez.alva@gmail.com>
2026-01-28 18:53:18 +0100

    first

---
+++ src/main/java/dev/jggutierrez/secureauthapi/SpringBootRestApiWithJwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package dev.jggutierrez.secureauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiWithJwtAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiWithJwtAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jggutierrez/secureauthapi/SpringBootRestApiWithJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jggutierrez.secureauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiWithJwtAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
